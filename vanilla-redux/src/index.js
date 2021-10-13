ce7f2ab6f2694d702eb9d711eab5fe8b3ad8cc7f
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-10-14 00:21:22 +0900

    Store and Reducer

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,21 +1,9 @@
+import {createStore} from "redux";
 const add =document.getElementById("add");
 const minus=document.getElementById("minus");
 const number=document.querySelector("span");
 
-let count=0;
-number.innerText=count;
-const updateText=()=>{
-
-  number.innerText=count;
-}
-
-const handleAdd=()=>{
-  count+=1;
-  updateText();
-}
-const handleMinus=()=>{
-  count-=1;
-  updateText();
-}
-add.addEventListener("click",handleAdd);
-minus.addEventListener("click",handleMinus);
+const countModifier=(count=0)=>{
+  return count;
+};
+const countStore=createStore(countModifier);
